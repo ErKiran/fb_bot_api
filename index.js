fe354eb24a5750fe36970d85a1fdcb5d5b0c69c4
Kiran Adhikari <kiruu1238@gmail.com>
2019-09-08 01:16:53 +0545

    Webhooks for Message

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const news = require('./routes/news');
 const horscope = require('./routes/horscope');
 const finance = require('./routes/finance');
 const jokes = require('./routes/jokes');
 const movies = require('./routes/movies');
+const webhook = require('./routes/webhook');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 app.use('/news', news);
@@ -15,7 +17,7 @@ app.use('/horscope', horscope);
 app.use('/finance', finance);
 app.use('/jokes', jokes);
 app.use('/movies', movies);
-
+app.use('/webhook', webhook)
 
 app.use('/', (req, res) => {
     res.json({ msg: 'Hello World' })
